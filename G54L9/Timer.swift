55c1c1f194cac2bb98400253fdf30e61d17fe5c9
Ivan Vasilevich <ivanvbesarab@gmail.com>
2017-05-23 20:08:33 +0300

    model `timer created

---
+++ G54L9/Timer.swift
@@ -0,0 +1,20 @@
+//
+//  Timer.swift
+//  G54L9
+//
+//  Created by Ivan Vasilevich on 5/23/17.
+//  Copyright © 2017 Smoosh Labs. All rights reserved.
+//
+
+import UIKit
+
+class MyTimer: NSObject {
+
+    func startTick(interval: TimeInterval) {
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(foo(timer:)), userInfo: nil, repeats: true)
+    }
+    
+    func foo(timer: Timer) {
+        print("tick")
+    }
+}
